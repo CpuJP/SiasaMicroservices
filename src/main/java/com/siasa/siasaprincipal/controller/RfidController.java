b1c13a2e399b2d1474282f957df54fbfa4e156f2
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2023-08-30 20:31:39 -0500

    ADD & FIX implementations in the CodigoU controller, improvements in business logic, new endpoints, improvement in documentation

--- src/main/java/com/siasa/siasaprincipal/controller/RfidController.java
+++ src/main/java/com/siasa/siasaprincipal/controller/RfidController.java
@@ -2,6 +2,8 @@ package com.siasa.siasaprincipal.controller;
 
 import com.siasa.siasaprincipal.dto.RfidDto;
 import com.siasa.siasaprincipal.service.RfidService;
+import io.swagger.v3.oas.annotations.OpenAPI31;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ public class RfidController {
     }
 
     //se crea el endpoint get principal de la entidad rfid donde se traen todos los datos de la BD
+    @Operation(summary = "Get all Rfid's")
     @GetMapping
     public ResponseEntity<List<RfidDto>> findAll() {
         //se utiliza el service de la implementacion en el endpoint para no cargar la
@@ -26,6 +29,7 @@ public class RfidController {
     }
 
     //se crea el endpoint post con la ruta /{id} donde se envia el id desde la api al server
+    @Operation(summary = "Find Rfid by Id")
     @PostMapping("/{id}")
     public ResponseEntity<RfidDto> findById(@PathVariable String id) {
         //se utiliza el service de la implementacion en el endpoint para no cargar la
@@ -35,12 +39,14 @@ public class RfidController {
 
     // Se crea el endpoint post principal para la creción de nuevos datos en BD de
     // la entidad Rfid
+    @Operation(summary = "Create a new Rfid")
     @PostMapping
     public ResponseEntity<RfidDto> create(@RequestBody RfidDto rfidDto) {
         return rfidService.create(rfidDto);
     }
 
     // Se crea el endpoint delete para borrar un carnet ya existente en la base de datos.
+    @Operation(summary = "Delete a Rfid by Id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         return rfidService.delete(id);
--- src/main/java/com/siasa/siasaprincipal/service/CodigoUServiceImpl.java
+++ src/main/java/com/siasa/siasaprincipal/service/CodigoUServiceImpl.java
@@ -1,19 +1,23 @@
 package com.siasa.siasaprincipal.service;
 
 import com.siasa.siasaprincipal.dto.CodigoUDto;
-import com.siasa.siasaprincipal.dto.RfidDto;
 import com.siasa.siasaprincipal.entity.CodigoU;
 import com.siasa.siasaprincipal.entity.Rfid;
+import com.siasa.siasaprincipal.exception.MessageBadRequestException;
+import com.siasa.siasaprincipal.exception.MessageConflictException;
 import com.siasa.siasaprincipal.exception.MessageNotContentException;
 import com.siasa.siasaprincipal.exception.MessageNotFoundException;
 import com.siasa.siasaprincipal.repository.CodigoURepository;
+import com.siasa.siasaprincipal.repository.RfidRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,10 +28,12 @@ public class CodigoUServiceImpl implements CodigoUService{
 
     private final CodigoURepository codigoURepository;
 
+    private final RfidRepository rfidRepository;
     private final ModelMapper modelMapper;
 
-    public CodigoUServiceImpl(CodigoURepository codigoURepository, ModelMapper modelMapper) {
+    public CodigoUServiceImpl(CodigoURepository codigoURepository, RfidRepository rfidRepository, ModelMapper modelMapper) {
         this.codigoURepository = codigoURepository;
+        this.rfidRepository = rfidRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -85,16 +91,77 @@ public class CodigoUServiceImpl implements CodigoUService{
 
     @Override
     public ResponseEntity<CodigoUDto> create(CodigoUDto codigoUDto) {
-        return null;
+        if (codigoUDto.getIdCodigoU().isEmpty() || codigoUDto.getPrimerNombre().isEmpty() || codigoUDto.getPrimerApellido().isEmpty() || codigoUDto.getRfidDto().getIdRfid().isEmpty()) {
+            log.warn("Los campos PrimerNombre, Primer Apellido, Codigo Universidad, Codigo Rfid son obligatorios");
+            throw new MessageBadRequestException("Los campos PrimerNombre, Primer Apellido, Codigo Universidad, Codigo Rfid son obligatorios");
+        }
+        if (codigoURepository.existsById(codigoUDto.getIdCodigoU())) {
+            log.warn(String.format("La persona con código %s ya se encuentra registrado", codigoUDto.getIdCodigoU()));
+            throw new MessageConflictException(String.format("La persona con código %s ya se encuentra registrado", codigoUDto.getIdCodigoU()));
+        } else if (codigoURepository.existsByRfidIdRfid(codigoUDto.getRfidDto().getIdRfid())){
+            log.warn(String.format("El código de carnet %s que está intentado vincular con el código %s ya se encuentra vinculado a otra persona", codigoUDto.getRfidDto().getIdRfid(), codigoUDto.getIdCodigoU()));
+            throw new MessageConflictException(String.format("El código de carnet %s que está intentado vincular con el código %s ya se encuentra vinculado a otra persona", codigoUDto.getRfidDto().getIdRfid(), codigoUDto.getIdCodigoU()));
+        } else if (!rfidRepository.existsById(codigoUDto.getRfidDto().getIdRfid())) {
+            log.warn("El carnet que se intenta asociar no existe");
+            throw new MessageBadRequestException("El carnet que se intenta asociar no existe");
+        } else {
+            CodigoU codigoU = modelMapper.map(codigoUDto, CodigoU.class);
+            CodigoU saveCodigoU = codigoURepository.save(codigoU);
+
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(saveCodigoU.getIdCodigoU())
+                    .toUri();
+            log.info("CodigoU creado exitosamente");
+            return ResponseEntity.created(location).body(modelMapper.map(saveCodigoU, CodigoUDto.class));
+        }
     }
 
     @Override
     public ResponseEntity<CodigoUDto> update(String id, CodigoUDto codigoUDto) {
-        return null;
+        CodigoU codigoU = codigoURepository.findById(id)
+                .orElseThrow(() -> new MessageNotFoundException(String.format("La persona con código %s no está registrada", id)));
+        codigoU.setPrimerNombre(codigoUDto.getPrimerNombre());
+        codigoU.setSegundoNombre(codigoUDto.getSegundoNombre());
+        codigoU.setPrimerApellido(codigoUDto.getPrimerApellido());
+        codigoU.setSegundoApellido(codigoUDto.getSegundoApellido());
+
+        Rfid rfidDelete = new Rfid();
+        rfidDelete.setIdRfid(codigoU.getRfid().getIdRfid());
+
+        Rfid rfid = new Rfid();
+        rfid.setIdRfid(codigoUDto.getRfidDto().getIdRfid());
+        codigoU.setRfid(rfid);
+
+
+
+        if (codigoU.getPrimerNombre().isEmpty() || codigoU.getPrimerApellido().isEmpty() || codigoU.getRfid().getIdRfid().isEmpty()) {
+            throw new MessageBadRequestException("El Primer Nombre, el Primer Apellido, y el código del Rfid son obligatorios");
+        } else if (!rfidRepository.existsById(codigoUDto.getRfidDto().getIdRfid())) {
+            log.warn("El carnet que se intenta asociar no existe");
+            throw new MessageBadRequestException("El carnet que se intenta asociar no existe");
+        }
+
+        try {
+            codigoURepository.save(codigoU);
+            TypeMap<CodigoU, CodigoUDto> typeMap = modelMapper.typeMap(CodigoU.class, CodigoUDto.class);
+            typeMap.addMapping(CodigoU::getRfid, CodigoUDto::setRfidDto);
+            codigoUDto = typeMap.map(codigoU);
+            rfidRepository.deleteById(rfidDelete.getIdRfid());
+            log.info(String.format("El carnet con código %s se eliminó con éxito", rfidDelete.getIdRfid()));
+            log.info("Actualización exitosa");
+            return ResponseEntity.ok(codigoUDto);
+        } catch (Exception e) {
+            log.warn(String.format("El código de carnet %s que está intentado vincular con el código %s ya se encuentra vinculado a otra persona", codigoUDto.getRfidDto().getIdRfid(), id));
+            throw new MessageConflictException(String.format("El código de carnet %s que está intentado vincular con el código %s ya se encuentra vinculado a otra persona", codigoUDto.getRfidDto().getIdRfid(), id));
+        }
+
     }
 
     @Override
     public ResponseEntity<Void> delete(String id) {
+        //TODO: cuando estén los demás controladores, venir y borrar en cascada a todas las entiades
         return null;
     }
 }
