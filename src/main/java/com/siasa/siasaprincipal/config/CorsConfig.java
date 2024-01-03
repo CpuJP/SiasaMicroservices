066a3ca6046994cdc2d8249d5e727edc883a3c5d
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-01-03 17:28:22 -0500

    ADD application.properties profiles for production environment and development environment

--- src/main/java/com/siasa/siasaprincipal/config/CorsConfig.java
+++ src/main/java/com/siasa/siasaprincipal/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.siasa.siasaprincipal.config;
 
 import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
