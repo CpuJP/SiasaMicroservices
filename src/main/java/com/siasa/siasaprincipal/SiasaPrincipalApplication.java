d02638f5c71ceaff1d62123f52fabb504ee25c8a
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-01-04 11:16:02 -0500

    UPDATE some class names and properties from the pom.xml

---
+++ src/main/java/com/siasa/siasaprincipal/SiasaPrincipalApplication.java
@@ -0,0 +1,12 @@
+package com.siasa.siasaprincipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiasaPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiasaPrincipalApplication.class, args);
+	}
+}
