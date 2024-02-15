6f30588814313953fbf293cdbbe9cc523f8c7f40
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-02-15 18:01:46 -0500

    FIRST commit

---
+++ src/main/java/com/siasa/principalfailover/SiasaPrincipalFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.siasa.principalfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiasaPrincipalFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiasaPrincipalFailoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siasa/principalfailover/SiasaPrincipalFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siasa.principalfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiasaPrincipalFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
