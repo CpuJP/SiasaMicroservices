01f73915cb84b681b4563241a6ea989aa2887bc0
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-02-15 15:06:11 -0500

    FIRST commit, CREATED project

---
+++ src/main/java/com/siasa/gateway/SiasaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.siasa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiasaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiasaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siasa/gateway/SiasaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siasa.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiasaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
