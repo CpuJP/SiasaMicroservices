908e8e8713ca6191771973db214adc08964261e5
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-02-15 11:56:23 -0500

    Add configuration for eureka client

---
+++ src/main/java/com/siasa/siasaprincipal/config/DiscoveryConfig.java
@@ -0,0 +1,9 @@
+package com.siasa.siasaprincipal.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryConfig {
+}
