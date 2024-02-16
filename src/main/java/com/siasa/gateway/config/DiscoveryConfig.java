8a0b5f2add38241a093804d03cb3d72821855d67
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-02-15 23:18:58 -0500

    FIX configuration CORS, UPDATE file application changes to .properties

---
+++ src/main/java/com/siasa/gateway/config/DiscoveryConfig.java
@@ -0,0 +1,9 @@
+package com.siasa.gateway.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryConfig {
+}
