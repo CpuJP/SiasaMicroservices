c632b127f810841f785f0a6e221997c6d1a49a7e
Juan Pablo Giraldo Collazos <cpujuanpis@gmail.com>
2024-03-03 15:38:29 -0500

    REMOVE cache in prod

--- src/main/java/com/siasa/principalfailover/config/CacheSiasaConfig.java
+++ src/main/java/com/siasa/principalfailover/config/CacheSiasaConfig.java
@@ -2,8 +2,10 @@ package com.siasa.principalfailover.config;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -22,6 +24,7 @@ public class CacheSiasaConfig {
     }
 
     @Bean
+    @Profile("dev")
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
@@ -29,5 +32,12 @@ public class CacheSiasaConfig {
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(redisCacheConfiguration)
                 .build();
+    }
+
+    @Bean
+    @Profile("!dev") // Activa este bean en todos los perfiles excepto "dev"
+    public CacheManager disableCacheManager() {
+        // Retorna un CacheManager vacío, lo que desactiva el caché
+        return new NoOpCacheManager();
     }
 }
